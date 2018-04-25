5f96fc05c62e262bb22cae5205c834ac19fedee1
waltershub <waltershub@gmail.com>
2018-04-25 13:19:26 -0400

    first erc721 token

--- contracts/HexColorToken.sol
+++ contracts/HexColorToken.sol
@@ -94,7 +94,4 @@ contract HexColorToken {
     event Transfer(address indexed _from, address indexed _to, uint256 _tokenId);
 
     event Approval(address indexed _owner, address indexed _approved, uint256 _tokenId);
-
-
-
 }
